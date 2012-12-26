15b66f58ceebe04969d82cabb85e4b1ec413f66d
Barry Zhong <zhongdj@gmail.com>
2012-12-27 01:38:46 +0800

    remove useless Test class

--- Madz.DatabaseMetaData/src/test/java/net/madz/crmp/db/core/DatabaseMetaDataTestSuite.java
+++ Madz.DatabaseMetaData/src/test/java/net/madz/crmp/db/core/DatabaseMetaDataTestSuite.java
@@ -1,14 +1,12 @@
 package net.madz.crmp.db.core;
 
-import net.madz.crmp.db.core.impl.DatabaseGeneratorTest;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 
 @RunWith(Suite.class)
-@SuiteClasses({DatabaseGeneratorTest.class})
+@SuiteClasses({})
 public class DatabaseMetaDataTestSuite {
     static {
         System.setProperty("net.madz.db.configuration", "/test-databases.xml");
