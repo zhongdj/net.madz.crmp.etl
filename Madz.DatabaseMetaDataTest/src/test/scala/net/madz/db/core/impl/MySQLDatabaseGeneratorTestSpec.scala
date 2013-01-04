78ba448aed77bd75ad5758bf03b3ebbfd68b6656
Barry Zhong <zhongdj@gmail.com>
2013-01-04 13:12:43 +0800

    add all sample DDLs to create tables with all mysql data type

--- Madz.DatabaseMetaDataTest/src/test/scala/net/madz/db/core/impl/MySQLDatabaseGeneratorTestSpec.scala
+++ Madz.DatabaseMetaDataTest/src/test/scala/net/madz/db/core/impl/MySQLDatabaseGeneratorTestSpec.scala
@@ -6,14 +6,14 @@ import scala.slick.session.Database
 import scala.slick.session.Database.threadLocalSession
 import org.scalatest.Assertions
 import org.scalatest.BeforeAndAfter
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.FunSpec
-import net.madz.crmp.db.core.AbsDatabaseGenerator
 import net.madz.crmp.db.core.impl.MySQLDatabaseGenerator
 import net.madz.crmp.db.metadata.SchemaMetaData
-import org.scalatest.BeforeAndAfterEach
+import scala.collection.mutable.ListBuffer
 
 class MySQLDatabaseGeneratorTestSpec extends FunSpec with BeforeAndAfterEach {
-  val show_tables_query = "show tables;"
+
   var conn: Connection = null
   var generator: MySQLDatabaseGenerator = null
 
@@ -35,7 +35,7 @@ class MySQLDatabaseGeneratorTestSpec extends FunSpec with BeforeAndAfterEach {
 
       val databaseName = "madz_empty_database_test"
       val schemaMetaData: SchemaMetaData = new SchemaMetaData(databaseName)
-      val generatedDbName = generator.generateDatabase(schemaMetaData, "madz_empty_database_test")
+      val generatedDbName = generator.generateDatabase(schemaMetaData, databaseName)
 
       Database.forURL(urlRoot + databaseName, user, password, driver = "com.mysql.jdbc.Driver") withSession {
         val q = Q.queryNA[String](show_tables_query)
@@ -58,7 +58,27 @@ class MySQLDatabaseGeneratorTestSpec extends FunSpec with BeforeAndAfterEach {
   describe("Generate correct columns") {
 
     it("should generate with all kinds of data type defined in JDBC specification") {
-      pending
+      val databaseName = "madz_database_test"
+      val schemaMetaData: SchemaMetaData = new SchemaMetaData(databaseName)
+      //add table meta data into schemaMetaData
+      val generatedDbName = generator.generateDatabase(schemaMetaData, databaseName)
+
+      Database.forURL(urlRoot + databaseName, user, password, driver = "com.mysql.jdbc.Driver") withSession {
+        val q = Q.queryNA[String](show_tables_query)
+        //Assertions.expectResult(0)(q.list().size)
+      }
+
+      val rs = conn.getMetaData().getTables(databaseName, null, null, Array("Table"))
+      val tables: ListBuffer[String] = ListBuffer()
+      while (rs.next())
+        tables += rs.getString("TABLE_NAME")
+      tables.foreach(tableName => {
+        val column = conn.getMetaData().getColumns(databaseName, null, tableName, null);
+        while (column.next()) {
+          println(column.getString("COLUMN_NAME") + "\t" + column.getInt("DATA_TYPE"))
+        }
+      })
+
     }
 
     it("should generate columns with modifier as NULLABLE(true or false)") {
@@ -118,5 +138,61 @@ class MySQLDatabaseGeneratorTestSpec extends FunSpec with BeforeAndAfterEach {
     }
 
   }
-
+  val show_tables_query = "SHOW tables;"
+  val create_table_with_all_data_types_DDL_1 = """
+    CREATE TABLE `table_with_all_data_types_p1` (
+      `BIT_COLUMN`                      BIT(1)                          DEFAULT NULL,
+      `BIT_PLUS_COLUMN`                 BIT(2)                          DEFAULT NULL,
+      `TINYINT_COLUMN`                  TINYINT(1)                      DEFAULT NULL,
+      `TINYINT_PLUS_COLUMN`             TINYINT(8)                      DEFAULT NULL,
+      `TINYINT_UNSIGNED_COLUMN`         TINYINT(8) UNSIGNED             DEFAULT NULL,
+      `BOOL_COLUMN`                     TINYINT(1)                      DEFAULT NULL,
+      `BOOLEAN_COLUMN`                  TINYINT(1)                      DEFAULT NULL,
+      `SMALLINT_COLUMN`                 SMALLINT(16)                    DEFAULT NULL,
+      `SMALLINT_UNSIGNED_COLUMN`        SMALLINT(16) UNSIGNED           DEFAULT NULL,
+      `MEDIUMINT_COLUMN`                MEDIUMINT(24)                   DEFAULT NULL,
+      `MEDIUMINT_UNSIGNED_COLUMN`       MEDIUMINT(24) UNSIGNED          DEFAULT NULL,
+      `INT_COLUMN`                      INT(32)                         DEFAULT NULL,
+      `INT_UNSIGNED_COLUMN`             INT(32) UNSIGNED                DEFAULT NULL,
+      `INTEGER_COLUMN`                  INTEGER(32)                     DEFAULT NULL,
+      `INTEGER_UNSIGNED_COLUMN`         INTEGER(32) UNSIGNED            DEFAULT NULL,
+      `BIGINT_COLUMN`                   BIGINT(64)                      DEFAULT NULL,                   
+      `BIGINT_UNSIGNED_COLUMN`          BIGINT(64) UNSIGNED             DEFAULT NULL,
+      `FLOAT_COLUMN`                    FLOAT(7,4)                      DEFAULT NULL,
+      `DOUBLE_COLUMN`                   DOUBLE PRECISION (64,30)        DEFAULT NULL,
+      `DOUBLE_PLUS_COLUMN`              DOUBLE PRECISION (128,30)       DEFAULT NULL,
+      `DECIMAL_COLUMN`                  DECIMAL                         DEFAULT NULL,
+      `DECIMAL_NO_SCALE_COLUMN`         DECIMAL(65, 0)                  DEFAULT NULL,
+      `DECIMAL_SCALE_COLUMN`            DECIMAL(65, 30)                 DEFAULT NULL,
+      `DATE_COLUMN`                     DATE                            DEFAULT NULL,
+      `DATETIME_COLUMN`                 DATETIME                        DEFAULT NULL,
+      `TIMESTAMP_COLUMN`                TIMESTAMP                       DEFAULT '2010-12-10 14:12:09',
+      `TIME_COLUMN`                     TIME                            DEFAULT NULL,
+      `YEAR_COLUMN`                     YEAR(2)                         DEFAULT NULL,
+      `YEAR_PLUS_COLUMN`                YEAR(4)                         DEFAULT NULL,
+      `CHAR_COLUMN`                     CHAR(255)                       DEFAULT NULL,
+      `VARCHAR_COLUMN`                  VARCHAR(65535)                  DEFAULT NULL,
+      `VARCHAR_BINARY_COLUMN`           VARCHAR(65535)  BINARY          DEFAULT NULL,
+      `BINARY_COLUMN`                   BINARY(255)                     DEFAULT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+  """
+  val create_table_with_all_data_types_DDL_2 = """
+    CREATE TABLE `table_with_all_data_types_p2` (
+      `VARBINARY_COLUMN`                VARBINARY(65532)                DEFAULT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+  """
+  val create_table_with_all_data_types_DDL_3 = """
+    CREATE TABLE `table_with_all_data_types_p3` (
+      `TINYBLOB_COLUMN`                 TINYBLOB                        DEFAULT NULL,
+      `TINYTEXT_COLUMN`                 TINYTEXT                        DEFAULT NULL,
+      `BLOB_COLUMN`                     BLOB                            DEFAULT NULL,
+      `TEXT_COLUMN`                     TEXT(65535)                     DEFAULT NULL,
+      `MEDIUMBLOB_COLUMN`               MEDIUMBLOB                      DEFAULT NULL,
+      `MEDIUMTEXT_COLUMN`               MEDIUMTEXT                      DEFAULT NULL,
+      `LONGBLOB_COLUMN`                 LONGBLOB                        DEFAULT NULL,
+      `LONGTEXT_COLUMN`                 LONGTEXT                        DEFAULT NULL,
+      `ENUM_COLUMN`                     ENUM('A','B','C')               DEFAULT NULL,
+      `SET_COLUMN`                      SET('HLJ','JX','BJ')            DEFAULT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+  """
 }
