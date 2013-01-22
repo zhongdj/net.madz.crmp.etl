14471c792ad9b2ca62fb21e61e818a21bd6c823c
Barry Zhong <zhongdj@gmail.com>
2013-01-22 16:33:29 +0800

    add single table parse test

---
+++ Madz.DatabaseMetaDataTest/src/test/scala/net/madz/db/core/impl/MySQLCommandLine.scala
@@ -0,0 +1,22 @@
+package net.madz.db.core.impl
+
+import scala.slick.session.Database
+import scala.collection.mutable.ListBuffer
+import scala.slick.jdbc.{ StaticQuery => Q }
+import scala.slick.session.Database
+import scala.slick.session.Database.threadLocalSession
+
+trait MySQLCommandLine extends MySQLConnector {
+
+  def exec(statement: String): Unit = {
+    Database.forURL(urlRoot, user, password, prop) withSession {
+      Q.queryNA[String](statement).execute
+    }
+  }
+  
+  def exec(statements: List[String]): Unit = {
+    Database.forURL(urlRoot, user, password, prop) withSession {
+      statements foreach (Q.queryNA[String](_) execute)
+    }
+  }
+}
--- Madz.DatabaseMetaDataTest/src/test/scala/net/madz/db/core/impl/MySQLSchemaMetaDataParserTest.scala
+++ Madz.DatabaseMetaDataTest/src/test/scala/net/madz/db/core/impl/MySQLSchemaMetaDataParserTest.scala
@@ -1,18 +1,16 @@
 package net.madz.db.core.impl
 
-import scala.collection.mutable.ListBuffer
-import scala.slick.jdbc.{ StaticQuery => Q }
-import scala.slick.session.Database
-import scala.slick.session.Database.threadLocalSession
-import org.scalatest.FunSpec
-import org.scalatest.BeforeAndAfterEach
-import net.madz.db.core.impl.mysql.MySQLSchemaMetaDataParserImpl
-import scala.slick.session.Database
 import java.sql.Connection
+import scala.slick.session.Database
 import org.scalatest.Assertions
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.FunSpec
+import net.madz.db.core.impl.mysql.MySQLSchemaMetaDataParserImpl
 import net.madz.db.metadata.DottedPath
+import net.madz.db.metadata.mysql.MySQLTableMetaData
+import net.madz.db.metadata.mysql.MySQLTableTypeEnum
 
-class MySQLSchemaMetaDataParserTest extends FunSpec with BeforeAndAfterEach with MySQLConnector {
+class MySQLSchemaMetaDataParserTest extends FunSpec with BeforeAndAfterEach with MySQLCommandLine {
 
   var conn: Connection = null
   var parser: MySQLSchemaMetaDataParserImpl = null
@@ -23,37 +21,34 @@ class MySQLSchemaMetaDataParserTest extends FunSpec with BeforeAndAfterEach with
   }
 
   override def afterEach {
-    Database.forURL(urlRoot, user, password, prop) withSession {
-      val q = Q.queryNA[String](drop_database_query)
-      q.execute
-    }
+    exec(drop_database_query)
     if (null != conn) conn.close()
   }
 
   describe("Parse an Empty Database") {
     it("should parse an empty database") {
 
-      Database.forURL(urlRoot, user, password, prop) withSession {
-        Q.queryNA[String](drop_database_query).execute
-        Q.queryNA[String](create_database_query).execute
-      }
+      exec(drop_database_query :: create_database_query :: Nil)
 
       val result = parser.parseSchemaMetaData()
       Assertions.expectResult(new DottedPath(database_name))(result getSchemaPath)
-      Assertions.expectResult(0)(result.getTables.size)
+      Assertions.expectResult(0)(result.getTables size)
     }
 
     it("should parse an empty database with correction database level default character set and default collation configuration") {
-      Database.forURL(urlRoot, user, password, prop) withSession {
-        Q.queryNA[String](drop_database_query).execute
-        Q.queryNA[String]("""
-            CREATE DATABASE `madz_database_parser_test`	DEFAULT CHARACTER SET = `gbk` DEFAULT COLLATE = `gbk_chinese_ci`;
-            """).execute
-      }
-      
+
+      exec(
+           drop_database_query 
+        :: """
+           CREATE DATABASE `madz_database_parser_test`	DEFAULT CHARACTER SET = `gbk` DEFAULT COLLATE = `gbk_chinese_ci`;
+           """  
+        :: Nil
+           )
+
       val result = parser.parseSchemaMetaData()
+     
       Assertions.expectResult(new DottedPath(database_name))(result getSchemaPath)
-      Assertions.expectResult(0)(result.getTables.size)
+      Assertions.expectResult(0)(result.getTables size)
       Assertions.expectResult("gbk")(result getCharSet)
       Assertions.expectResult("gbk_chinese_ci")(result getCollation)
 
@@ -62,8 +57,31 @@ class MySQLSchemaMetaDataParserTest extends FunSpec with BeforeAndAfterEach with
   }
 
   describe("Parse a single table Database") {
-    it("should parse correct charsetEncoding with MySQL Parser") {
-      pending
+    
+    it("should parse correct tableName, charsetEncoding, collation, and storage engine with MySQL Parser") {
+      exec(
+          drop_database_query
+        :: """
+           CREATE DATABASE `madz_database_parser_test`	DEFAULT CHARACTER SET = `gbk` DEFAULT COLLATE = `gbk_chinese_ci`;
+           """
+        :: "USE `madz_database_parser_test`;"
+        :: """
+           CREATE TABLE `single_table_test` (
+              name VARCHAR(64) DEFAULT NULL
+           ) ENGINE=`InnoDB` DEFAULT CHARACTER SET=`utf8` DEFAULT COLLATE=`utf8_unicode_ci`;
+           """
+        :: Nil
+          )
+          
+       val result = parser.parseSchemaMetaData()
+       
+       Assertions.expectResult(1)(result.getTables() size)
+       val table = result.getTables().toArray[MySQLTableMetaData](Array[MySQLTableMetaData]())(0)
+       Assertions.expectResult("madz_database_parser_test")(table getTableName)
+       Assertions.expectResult("InnoDB" toLowerCase)(table getEngine)
+       Assertions.expectResult("utf8" toLowerCase)(table getCharacterSet)
+       Assertions.expectResult("utf8_unicode_ci" toLowerCase)(table getCollation)
+       Assertions.expectResult(MySQLTableTypeEnum.base_table)(table getTableType)
     }
 
     it("should parse correct storage engine with MySQL Parser") {
