7306d776aa74cd7b2d88d8c95df0ff40fb91d659
Tracy <lu-hailing@hotmail.com>
2013-02-01 12:30:52 +0800

    Refactoring build method of MySQLSchemaMetaDataBuilderImpl
    Extracted setCharacterSetAndCollation, getTableNames,
    getFkNamesOfTables methods.

--- Madz.DatabaseMetaData/src/main/java/net/madz/db/core/meta/mutable/mysql/impl/MySQLSchemaMetaDataBuilderImpl.java
+++ Madz.DatabaseMetaData/src/main/java/net/madz/db/core/meta/mutable/mysql/impl/MySQLSchemaMetaDataBuilderImpl.java
@@ -11,8 +11,6 @@ import java.util.List;
 import java.util.Map;
 
 import net.madz.db.core.meta.DottedPath;
-import net.madz.db.core.meta.immutable.MetaData;
-import net.madz.db.core.meta.immutable.SchemaMetaData;
 import net.madz.db.core.meta.immutable.mysql.MySQLColumnMetaData;
 import net.madz.db.core.meta.immutable.mysql.MySQLForeignKeyMetaData;
 import net.madz.db.core.meta.immutable.mysql.MySQLIndexMetaData;
@@ -26,6 +24,7 @@ import net.madz.db.core.meta.mutable.mysql.MySQLForeignKeyMetaDataBuilder;
 import net.madz.db.core.meta.mutable.mysql.MySQLIndexMetaDataBuilder;
 import net.madz.db.core.meta.mutable.mysql.MySQLSchemaMetaDataBuilder;
 import net.madz.db.core.meta.mutable.mysql.MySQLTableMetaDataBuilder;
+import net.madz.db.utils.MessageConsts;
 import net.madz.db.utils.ResourceManagementUtils;
 
 public class MySQLSchemaMetaDataBuilderImpl
@@ -36,66 +35,36 @@ public class MySQLSchemaMetaDataBuilderImpl
     private String charSet;
     private String collation;
 
-    public MySQLSchemaMetaDataBuilderImpl(DottedPath schemaPath) throws SQLException {
+    public MySQLSchemaMetaDataBuilderImpl(final DottedPath schemaPath) throws SQLException {
         super(schemaPath);
     }
 
     public MySQLSchemaMetaDataBuilder build(Connection conn) throws SQLException {
-        System.out.println("Mysql schema builder");
-        // Get MySql information
         final Statement stmt = conn.createStatement();
+        // We should keep the context of querying in information_schema
+        // database.
         stmt.executeQuery("USE information_schema;");
-        ResultSet rs = null;
-        try {
-            rs = stmt.executeQuery("SELECT * FROM schemata WHERE schema_name = '" + schemaPath.getName() + "'");
-            while ( rs.next() && rs.getRow() == 1 ) {
-                charSet = rs.getString("default_character_set_name");
-                collation = rs.getString("default_collation_name");
-            }
-        } finally {
-            ResourceManagementUtils.closeResultSet(rs);
-        }
-        final List<String> tableNames = new LinkedList<String>();
-        try {
-            // Construct Table builders and append
-            rs = stmt.executeQuery("SELECT * FROM tables WHERE table_schema = '" + schemaPath.getName() + "'");
-            while ( rs.next() ) {
-                tableNames.add(rs.getString("table_name"));
-            }
-        } finally {
-            ResourceManagementUtils.closeResultSet(rs);
-        }
-        // For database without tables
+        setCharacterSetAndCollation(stmt);
+        
+        final List<String> tableNames = getTableNames(stmt);
+        // For database without tables, just return
         if ( 0 >= tableNames.size() ) {
             return this;
         }
+        // Build all tables
         final Map<String, MySQLTableMetaDataBuilder> tableBuilders = new HashMap<String, MySQLTableMetaDataBuilder>();
         for ( final String name : tableNames ) {
             final MySQLTableMetaDataBuilder table = new MySQLTableMetaDataBuilderImpl(this, name).build(conn);
             tableBuilders.put(name, table);
             appendTableMetaDataBuilder(table);
         }
-        // Construct foreignKeys
-        final Map<String, LinkedList<String>> fkNamesOfTables = new HashMap<String, LinkedList<String>>();
-        for ( final String tableName : tableNames ) {
-            final LinkedList<String> fks = new LinkedList<String>();
-            try {
-                rs = stmt.executeQuery("SELECT * FROM referential_constraints WHERE constraint_schema = '" + this.schemaPath.getName() + "' AND table_name = '"
-                        + tableName + "';");
-                while ( rs.next() ) {
-                    fks.add(rs.getString("constraint_name"));
-                }
-            } finally {
-                ResourceManagementUtils.closeResultSet(rs);
-            }
-            if ( fks.size() > 0 ) {
-                fkNamesOfTables.put(tableName, fks);
-            }
-        }
+        
+        final Map<String, LinkedList<String>> fkNamesOfTables = getFkNamesOfTables(stmt, tableNames);
         // For database with no foreign keys
         if ( 0 >= fkNamesOfTables.size() ) {
             return this;
         }
+        // Build all foreign keys
         for ( String tableName : fkNamesOfTables.keySet() ) {
             final LinkedList<String> fks = fkNamesOfTables.get(tableName);
             for ( String name : fks ) {
@@ -150,5 +119,56 @@ public class MySQLSchemaMetaDataBuilderImpl
     @Override
     public MySQLSchemaMetaData getMetaData() {
         return this.createMetaData();
+    }
+
+    public Map<String, LinkedList<String>> getFkNamesOfTables(final Statement stmt, final List<String> tableNames) throws SQLException {
+        ResultSet rs = null;
+        final Map<String, LinkedList<String>> fkNamesOfTables = new HashMap<String, LinkedList<String>>();
+        for ( final String tableName : tableNames ) {
+            final LinkedList<String> fks = new LinkedList<String>();
+            try {
+                rs = stmt.executeQuery("SELECT * FROM referential_constraints WHERE constraint_schema = '" + this.schemaPath.getName() + "' AND table_name = '"
+                        + tableName + "';");
+                while ( rs.next() ) {
+                    fks.add(rs.getString("constraint_name"));
+                }
+            } finally {
+                ResourceManagementUtils.closeResultSet(rs);
+            }
+            if ( fks.size() > 0 ) {
+                fkNamesOfTables.put(tableName, fks);
+            }
+        }
+        return fkNamesOfTables;
+    }
+
+    public List<String> getTableNames(final Statement stmt) throws SQLException {
+        ResultSet rs = null;
+        final List<String> tableNames = new LinkedList<String>();
+        try {
+            // Construct Table builders and append
+            rs = stmt.executeQuery("SELECT * FROM tables WHERE table_schema = '" + schemaPath.getName() + "'");
+            while ( rs.next() ) {
+                tableNames.add(rs.getString("table_name"));
+            }
+        } finally {
+            ResourceManagementUtils.closeResultSet(rs);
+        }
+        return tableNames;
+    }
+
+    public void setCharacterSetAndCollation(final Statement stmt) throws SQLException {
+        ResultSet rs = null;
+        try {
+            rs = stmt.executeQuery("SELECT * FROM schemata WHERE schema_name = '" + schemaPath.getName() + "'");
+            if ( rs.next() && rs.getRow() == 1 ) {
+                charSet = rs.getString("default_character_set_name");
+                collation = rs.getString("default_collation_name");
+            } else {
+                throw new IllegalStateException(MessageConsts.ONLY_ONE_SCHEMA_INFORMATION_IS_OK);
+            }
+        } finally {
+            ResourceManagementUtils.closeResultSet(rs);
+        }
     }
 }
--- Madz.DatabaseMetaData/src/main/java/net/madz/db/utils/MessageConsts.java
+++ Madz.DatabaseMetaData/src/main/java/net/madz/db/utils/MessageConsts.java
@@ -2,6 +2,7 @@ package net.madz.db.utils;
 
 public class MessageConsts {
 
-    public static final String ArgumentShouldNotBeNull = "Argument should not be null, please check.";
+    public static final String ARGUMENT_SHOULD_NOT_BE_NULL = "Argument should not be null, please check.";
     public static final String CONFIGURE_DATABASE_INFO = "Please make sure configure souce database information.";
+    public static final String ONLY_ONE_SCHEMA_INFORMATION_IS_OK = "Only one schema information should be returned. Please check your SQL statement.";
 }
