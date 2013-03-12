0ff254602843f1aa2a80872e6519acf0bedf2b98
Tracy <lu-hailing@hotmail.com>
2013-03-12 23:54:59 +0800

    Fixed bug: Wrongly appended character set or collate when it is null

--- Madz.DatabaseMetaData/src/main/java/net/madz/db/core/meta/immutable/mysql/datatype/MySQLCharBase.java
+++ Madz.DatabaseMetaData/src/main/java/net/madz/db/core/meta/immutable/mysql/datatype/MySQLCharBase.java
@@ -5,51 +5,55 @@ import net.madz.db.utils.Utilities;
 
 public abstract class MySQLCharBase implements DataType {
 
-	private final long length;
-	private final String charsetName;
-	private final String collationName;
+    private final long length;
+    private final String charsetName;
+    private final String collationName;
 
-	public MySQLCharBase(long length) {
-		this(length, null, null);
-	}
+    public MySQLCharBase(long length) {
+        this(length, null, null);
+    }
 
-	public MySQLCharBase(long length, String charsetName, String collationName) {
-		super();
-		Utilities.validateLength(length);
-		this.length = length;
-		this.charsetName = charsetName;
-		this.collationName = collationName;
-	}
+    public MySQLCharBase(long length, String charsetName, String collationName) {
+        super();
+        Utilities.validateLength(length);
+        this.length = length;
+        this.charsetName = charsetName;
+        this.collationName = collationName;
+    }
 
-	public long getLength() {
-		return length;
-	}
+    public long getLength() {
+        return length;
+    }
 
-	public String getCharsetName() {
-		return charsetName;
-	}
+    public String getCharsetName() {
+        return charsetName;
+    }
 
-	public String getCollationName() {
-		return collationName;
-	}
+    public String getCollationName() {
+        return collationName;
+    }
 
-	@Override
-	public void build(MySQLColumnMetaDataBuilder builder) {
-		builder.setSqlTypeName(getName());
-		builder.setCharacterMaximumLength(getLength());
-		builder.setCharacterSet(this.charsetName);
-		builder.setCollationName(this.collationName);
-		final StringBuilder result = new StringBuilder();
-		result.append(getName());
-		if (0 < getLength()) {
-			result.append("(");
-			result.append(getLength());
-			result.append(")");
-		}
-		result.append(" CHARACTER SET ");
-		result.append(charsetName);
-		result.append(" COLLATE ");
-		result.append(collationName);
-		builder.setColumnType(result.toString());
-	}
+    @Override
+    public void build(MySQLColumnMetaDataBuilder builder) {
+        builder.setSqlTypeName(getName());
+        builder.setCharacterMaximumLength(getLength());
+        builder.setCharacterSet(this.charsetName);
+        builder.setCollationName(this.collationName);
+        final StringBuilder result = new StringBuilder();
+        result.append(getName());
+        if ( 0 < getLength() ) {
+            result.append("(");
+            result.append(getLength());
+            result.append(")");
+        }
+        if ( null != charsetName && 0 < charsetName.length() ) {
+            result.append(" CHARACTER SET ");
+            result.append(charsetName);
+        }
+        if ( null != collationName && 0 < collationName.length() ) {
+            result.append(" COLLATE ");
+            result.append(collationName);
+        }
+        builder.setColumnType(result.toString());
+    }
 }
