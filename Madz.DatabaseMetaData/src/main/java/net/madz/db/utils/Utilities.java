6b6cdcdf3b94d02b98edadfd512ee455564d330a
Tracy <lu-hailing@hotmail.com>
2013-03-25 16:17:25 +0800

    Added validateInputValueNotNull method into Utils

--- Madz.DatabaseMetaData/src/main/java/net/madz/db/utils/Utilities.java
+++ Madz.DatabaseMetaData/src/main/java/net/madz/db/utils/Utilities.java
@@ -1,5 +1,6 @@
 package net.madz.db.utils;
 
+
 public class Utilities {
 
     public static void validateLength(long length) {
@@ -7,11 +8,17 @@ public class Utilities {
             throw new IllegalArgumentException(MessageConsts.LENGTH_MUST_BE_GREATER_THAN_ZERO);
         }
     }
-    
+
     public static String handleSpecialCharacters(String value) {
         if ( value.contains("'") ) {
             return value.replace("'", "\\'");
         }
         return value;
+    }
+
+    public static void validateInputValueNotNull(Object value) {
+        if ( null == value ) {
+            throw new IllegalArgumentException(MessageConsts.ARGUMENT_SHOULD_NOT_BE_NULL);
+        }
     }
 }
