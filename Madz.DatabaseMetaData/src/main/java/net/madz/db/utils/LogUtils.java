78d269199634f73aa4231d172bd4f72b47d3f4eb
TracyLu <lu-hailing@hotmail.com>
2013-04-14 20:34:40 +0800

    Configured Log4j for output exception information

--- Madz.DatabaseMetaData/src/main/java/net/madz/db/utils/LogUtils.java
+++ Madz.DatabaseMetaData/src/main/java/net/madz/db/utils/LogUtils.java
@@ -1,8 +1,13 @@
 package net.madz.db.utils;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class LogUtils {
+
+    static {
+        PropertyConfigurator.configure("conf/properties/ServerWithLog4j.properties");
+    }
 
     @SuppressWarnings("rawtypes")
     public static void debug(Class cls, String message) {
