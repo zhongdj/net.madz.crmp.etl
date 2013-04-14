c0da78faa173563758129b589501009fb18b14aa
TracyLu <lu-hailing@hotmail.com>
2013-04-14 22:29:57 +0800

    Fixed bug: For access database, ResultSet has not implemented isClosed method.

--- Madz.DatabaseMetaData/src/main/java/net/madz/db/utils/ResourceManagementUtils.java
+++ Madz.DatabaseMetaData/src/main/java/net/madz/db/utils/ResourceManagementUtils.java
@@ -5,9 +5,13 @@ import java.sql.SQLException;
 
 public class ResourceManagementUtils {
 
-    public static void closeResultSet(ResultSet rs) throws SQLException {
-        if ( null != rs && !rs.isClosed() ) {
-            rs.close();
+    public static void closeResultSet(ResultSet rs) {
+        if ( null != rs ) {
+            try {
+                rs.close();
+            } catch (SQLException ignored) {
+                LogUtils.debug(ResourceManagementUtils.class, ignored.getMessage());
+            }
         }
     }
 }
